e69de6cdbc4b076eb27ee0393be5fe2301762545
Keturah Howard <keturah.dev@gmail.com>
2020-04-19 16:28:48 -0700

    create another ticket for ticket list

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -3,11 +3,17 @@ import Ticket from './Ticket';
 
 function TicketList() {
   return (
-    <Ticket 
-    location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"
-    />
+    <React.Fragment>
+      <Ticket 
+      location="3A"
+        names="Thato and Haley"
+        issue="Firebase will not save record!"
+      />
+      <Ticket
+          location="4B"
+          names="Sleater and Kinney"
+          issue="Prop types are throwing an error."/>
+    </React.Fragment>
   );
 }
 
