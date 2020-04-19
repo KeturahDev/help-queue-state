1cfab6ffb084178a80d7d70177ebe8cf0bc61450
Keturah Howard <keturah.dev@gmail.com>
2020-04-19 16:14:31 -0700

    create basic outline of static react skeleton

---
+++ src/components/Ticket.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Ticket() {
+  const name="ThisGuy"
+  const name2="ThatDude"
+  return (
+    <ReactFragment>
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+    </ReactFragment>
+  )
+}
+
+export default Ticket;
