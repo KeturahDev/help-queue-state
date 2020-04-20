abce7b7697fed80f77fa092deb89f205cd15a7bb
Keturah Howard <keturah.dev@gmail.com>
2020-04-19 17:38:55 -0700

    add propType strictness

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Ticket(props) {
   return (
@@ -9,6 +10,12 @@ function Ticket(props) {
       <hr/>
     </React.Fragment>
   )
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string
 }
 
 export default Ticket;
